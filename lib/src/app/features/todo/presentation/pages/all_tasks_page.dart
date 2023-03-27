9243f195339e1e7e2c1d8ef98c31e560cdc9d668
AJ8454 <ajaypangare8454@gmail.com>
2023-03-27 17:36:57 +0530

    Home Done

--- lib/src/app/features/todo/presentation/pages/all_tasks_page.dart
+++ lib/src/app/features/todo/presentation/pages/all_tasks_page.dart
@@ -1,5 +1,6 @@
 import 'package:architech_todo/src/app/features/todo/presentation/bloc/todo_bloc.dart';
 import 'package:architech_todo/src/app/features/todo/presentation/pages/edit_task.dart';
+import 'package:architech_todo/src/core/responsive/app_responsive.dart';
 import 'package:architech_todo/src/core/routes/app_navigator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -30,48 +31,56 @@ class _AllTaskPageState extends State<AllTaskPage> {
           if (state is TodoLoadingState) {
             return const Center(child: CircularProgressIndicator());
           } else if (state is TodoLoadedState) {
-            return ListView.separated(
-              padding: const EdgeInsets.only(top: 10, bottom: 20),
-              itemBuilder: (context, index) {
-                final todo = state.todos[index];
-                return Card(
-                  child: ListTile(
-                    title: Text(todo.title),
-                    trailing: SizedBox(
-                      width: 100,
-                      child: Row(
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              AppNavigator.pushWithData(
-                                  context,
-                                  EditTasksPage.route,
-                                  {"fromEdit": true, "todo": todo});
-                            },
-                            icon: Icon(
-                              Icons.edit,
-                              color: Colors.redAccent.shade100,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              BlocProvider.of<TodoBloc>(context)
-                                  .add(DeleteTaskEvent(todo.id!));
-                            },
-                            icon: Icon(
-                              Icons.delete,
-                              color: Colors.redAccent.shade100,
-                            ),
-                          ),
-                        ],
-                      ),
+            return state.todos.isEmpty
+                ? Center(
+                    child: Text(
+                      "No Tasks yet....",
+                      style: AppResponsive.responsiveTextStyle(context),
                     ),
-                  ),
-                );
-              },
-              separatorBuilder: (context, index) => const SizedBox(height: 5),
-              itemCount: state.todos.length,
-            );
+                  )
+                : ListView.separated(
+                    padding: const EdgeInsets.only(top: 10, bottom: 20),
+                    itemBuilder: (context, index) {
+                      final todo = state.todos[index];
+                      return Card(
+                        child: ListTile(
+                          title: Text(todo.title),
+                          trailing: SizedBox(
+                            width: 100,
+                            child: Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    AppNavigator.pushWithData(
+                                        context,
+                                        EditTasksPage.route,
+                                        {"fromEdit": true, "todo": todo});
+                                  },
+                                  icon: Icon(
+                                    Icons.edit,
+                                    color: Colors.redAccent.shade100,
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    BlocProvider.of<TodoBloc>(context)
+                                        .add(DeleteTaskEvent(todo.id!));
+                                  },
+                                  icon: Icon(
+                                    Icons.delete,
+                                    color: Colors.redAccent.shade100,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) =>
+                        const SizedBox(height: 5),
+                    itemCount: state.todos.length,
+                  );
           } else {
             return const SizedBox();
           }
